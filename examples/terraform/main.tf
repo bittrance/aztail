2f70a2554c89ddd2592251669ad9246e2c8e2b98
Anders Qvist <anders.qvist@xenit.se>
2021-11-28 22:45:12 +0100

    Terraform Function clean-up.
    Signed-off-by: Anders Qvist <anders.qvist@xenit.se>

--- examples/terraform/main.tf
+++ examples/terraform/main.tf
@@ -1,31 +1,9 @@
-# resource "azurerm_key_vault_access_policy" "kv_policy" {
-#   key_vault_id = azurerm_key_vault.kv.id
-#   tenant_id    = azurerm_function_app.function_app.identity.0.tenant_id
-#   object_id    = azurerm_function_app.function_app.identity.0.principal_id
-#   secret_permissions = [
-#     "Get"
-#   ]
-# }
-
-provider azurerm {
+provider "azurerm" {
   features {}
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
-  name                = "plan-aztail-function"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  kind                = "elastic"
-  reserved            = true
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
 resource "azurerm_resource_group" "rg" {
-  name = "rg-aztail-example-function"
+  name     = "aztail-example-function"
   location = "West Europe"
 }
 
@@ -35,6 +13,19 @@ resource "azurerm_storage_account" "storage_account" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "plan-aztail-function"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
 }
 
 resource "azurerm_application_insights" "application_insights" {
@@ -48,23 +39,21 @@ resource "azurerm_function_app" "function_app" {
   name                       = "func-aztail-example-function"
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
+  version                    = "~3"
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
   app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
   app_settings = {
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
     "FUNCTIONS_WORKER_RUNTIME"       = "node",
     "WEBSITE_RUN_FROM_PACKAGE"       = "",
   }
-  identity {
-    type = "SystemAssigned"
-  }
+
   os_type = "linux"
   site_config {
     linux_fx_version          = "node|14"
     use_32_bit_worker_process = false
   }
-  storage_account_name       = azurerm_storage_account.storage_account.name
-  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
-  version                    = "~3"
 
   lifecycle {
     ignore_changes = [
@@ -72,8 +61,3 @@ resource "azurerm_function_app" "function_app" {
     ]
   }
 }
-
-# resource "azurerm_app_service_virtual_network_swift_connection" "connection" {
-#   app_service_id = azurerm_function_app.function_app.id
-#   subnet_id      = data.azurerm_subnet.subnet.id
-# }
